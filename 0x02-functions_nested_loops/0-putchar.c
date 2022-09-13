0cfabb50ff0c2148b2c8a2417475af9e2104cf68
Fesobi Olumide Phillip <fesobiolumide@gmail.com>
2022-09-13 04:08:24 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,19 @@
-#include "main.h"
+#include "putchar.c"
 
 /**
- * main - the driver function print _putchar followed on a new line
- * Return: always 0
+ * main - Entry point
+ * Return:0
  */
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+char c[10] = "Putchar";
+int i = 0;
+while (i < 9)
+{
+_putchar(c[i]);
+i++;
+}
+_putchar('\n');
+return (0);
 }

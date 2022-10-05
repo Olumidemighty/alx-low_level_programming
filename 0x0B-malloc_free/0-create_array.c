9f48d3f5f62e70e21ee9ff0c9e90e0121adad893
Fesobi Olumide Phillip <fesobiolumide@gmail.com>
2022-10-05 08:12:48 -0700

    hotfix

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,17 +9,18 @@
  * @c: character to init array 
  * Return: char pointer  
  */
-char *create_array(unsigned int size, char c) 
+char *create_array(unsigned int size, char c)
 {
-	char *char_arr; 
-	 unsigned int i; 
+	char *char_arr;
+	unsigned int i;
 
-	  if (size == 0)
-		   return (NULL);
-	   char_arr = malloc(sizeof(char) * size); 
-	    if (char_arr == NULL) 
-		     return (NULL);  
-	    for (i = 0; i < size; i++)
-		     char_arr[i] = c;  
-	     return (char_arr); 
+	if (size == 0)
+		return (NULL);
+	char_arr = malloc(sizeof(char) * size);
+	if (char_arr == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		char_arr[i] = c;
+	return (char_arr);
 }
+
